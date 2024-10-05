c95d83d53f7e98ff338b060a521728208bad20a9
Ankith Gopi Krishna Avula <aavula1@charlotte.edu>
2024-10-04 21:20:59 -0400

    Update TopBar.jsx

--- components/topBar/TopBar.jsx
+++ components/topBar/TopBar.jsx
@@ -1,28 +1,54 @@
-import React from 'react';
-import {
-  AppBar, Toolbar, Typography
-} from '@mui/material';
-import './TopBar.css';
+import React, { useState, useEffect } from 'react';
+import { AppBar, Toolbar, Typography } from '@mui/material';
+import { withRouter } from 'react-router-dom';
+import FetchModel from '../../lib/fetchModelData';
 
-/**
- * Define TopBar, a React componment of project #5
- */
-class TopBar extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+function TopBar(props) {
+    const [version, setVersion] = useState('');
 
-  render() {
+    useEffect(() => {
+        FetchModel('http://localhost:3000/test/info')
+            .then((response) => {
+                const versionNumber = response.data.load_date_time;
+                setVersion(versionNumber);
+            })
+            .catch((error) => {
+                console.error('Error fetching version number:', error);
+            });
+    }, []);
+
+    const pathname = props.location.pathname;
+    const userId = pathname.includes('/users/') ? pathname.split('/users/')[1] : null;
+    const photo = pathname.includes('/photos/') ? pathname.split('/photos/')[1] : null;
+
     return (
-      <AppBar className="topbar-appBar" position="absolute">
-        <Toolbar>
-          <Typography variant="h5" color="inherit">
-              This is the TopBar component
-          </Typography>
-        </Toolbar>
-      </AppBar>
+        <>
+            {/* AppBar with fixed positioning */}
+            <AppBar className="topbar-appBar" position="fixed">
+                <Toolbar style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Typography variant="h5" color="inherit">
+                        Bastions
+                    </Typography>
+                    <Typography variant="h5" color="inherit">
+                        {userId
+                            ? `Details of ${models.userModel(userId).first_name}`
+                            : photo
+                            ? `Photos of ${models.userModel(photo).first_name}`
+                            : ''}
+                    </Typography>
+                    <Typography variant="body2" color="inherit">
+                        Version: {version}
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+
+            {/* Add margin to avoid content overlap */}
+            <div className="main-content">
+                {/* This is where the rest of your content goes */}
+                {props.children}
+            </div>
+        </>
     );
-  }
 }
 
-export default TopBar;
+export default withRouter(TopBar);
